284bdc7efbbdd8c40c737ac13b235afeb51db8e0
flowers1225 <438052770@qq.com>
2015-01-30 16:30:27 +0800

    mh

---
+++ demo/mh/js/s.js
@@ -0,0 +1,49 @@
+"use strict"
+
+;(function(){
+
+    var swipe = null;
+    var sugar = null;
+    var imageLoader = new LoadImages();
+
+    function pageControl(index,elem){
+        
+        sugar.fireOut(elem);
+        sugar.fireIn(elem);
+
+    }
+
+    //添加其他loading的内容
+    var preloadimgs = [
+        './img/bg_index.jpg',
+        './img/bg_section_2.jpg',
+        './img/bg_section_3.jpg',
+        './img/bg_section_4.jpg',
+        './img/bg_section_5.jpg',
+        './img/bg_section_6.jpg'
+    ];
+
+    var init = function(){
+        
+        imageLoader.load(preloadimgs);
+
+        imageLoader.addEventListener('onLoad', function(){
+        
+            $('.m-load').remove();
+
+            //初始化rotate
+            sugar = new Sugar();
+            sugar.fireIn($('.m-section-index'));
+            
+            //初始化swipe
+            swipe = Swipe(document.querySelector('.m-wrap'),{
+                continuous: false,
+                callback: pageControl,
+                transitionEnd: function(index, elem) {}
+            });
+        
+        });    
+	}
+
+    init();
+})();
